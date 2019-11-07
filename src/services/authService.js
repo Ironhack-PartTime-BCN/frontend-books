fc64effe6876546cb710cb8a89af8760c29ca087
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2019-11-07 17:23:04 +0100

    update env

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,33 +1,33 @@
-import axios from 'axios';
+import axios from "axios";
 
 class AuthService {
   constructor() {
     this.auth = axios.create({
-      baseURL: 'http://localhost:3000',
+      baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
       withCredentials: true
-    })
+    });
   }
 
   signup(user) {
     const { username, password } = user;
-    return this.auth.post('/signup', {username, password})
+    return this.auth
+      .post("/signup", { username, password })
       .then(({ data }) => data);
   }
 
   login(user) {
     const { username, password } = user;
-    return this.auth.post('/login', {username, password})
+    return this.auth
+      .post("/login", { username, password })
       .then(({ data }) => data);
   }
 
   logout() {
-    return this.auth.get('/logout', {})
-      .then(response => response.data)
+    return this.auth.get("/logout", {}).then(response => response.data);
   }
 
   me(user) {
-    return this.auth.get('/me')
-    .then(response => response.data)
+    return this.auth.get("/me").then(response => response.data);
   }
 }
 
